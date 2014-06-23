f3fd9ac4c7fd88720d7888c96898cbeeef0043f4
Andy Gill <andygill@ku.edu>
2014-06-23 12:25:00 -0500

    Adding SORT_BY implementation

--- crud/Data/CRUD.hs
+++ crud/Data/CRUD.hs
@@ -30,7 +30,7 @@ import qualified Data.HashMap.Strict as HashMap
 import Data.HashMap.Strict (HashMap)
 import Control.Applicative
 import Data.Char (isSpace, isDigit, chr)
-import Data.List (foldl')
+import Data.List (foldl', sortBy)
 import Data.Text (Text, pack)
 import Control.Monad
 import qualified Data.Text as Text
@@ -38,6 +38,7 @@ import Control.Concurrent.STM
 import Control.Concurrent
 import Control.Exception
 import System.IO
+import Data.Scientific
 
 ------------------------------------------------------------------------------------
 -- | A CRUD is a OO-style database Table of typed rows, with getters and setters. 
@@ -285,15 +286,51 @@ tableUpdate (Shutdown msg)              = id
 
 -- SQL-style selectors
 data SELECT = SELECT [Text]                    -- ^ Only return listed fields
-            | SORTBY Text                      -- ^ sort on a field, field needs to be present
-            | LIMIT Int                        -- ^ Only give n answers
-            | DROP  Int                        -- ^ ignore the first n answers
+            | SORT_BY Text [SORT_MOD]          -- ^ sort on a field
+            | TAKE Int                         -- ^ Only give n answers
+            | DROP Int                         -- ^ ignore the first n answers
+            deriving (Eq, Ord, Show, Read)
 
 select :: [SELECT] -> [Row] -> [Row]
 select ss rows = foldl sel rows ss
-  where sel rows (SELECT ns) = fmap (\ row -> HashMap.fromList [ (k,v) | (k,v) <- HashMap.toList row, k `elem` ns]) rows
-        sel rows (SORTBY ns) = rows
-        sel rows (DROP n)    = drop n rows
-        sel rows (LIMIT n)   = Prelude.take n rows
-        
-        
+  where sel rows (SELECT ns)     = fmap (\ row -> HashMap.fromList [ (k,v) | (k,v) <- HashMap.toList row, k `elem` ns]) rows
+        sel rows (SORT_BY nm ms) 
+                | NUM `elem` ms && DESC `elem` ms = sort_by desc $ prep num 
+                | NUM `elem` ms                   = sort_by desc $ prep num 
+                |                  DESC `elem` ms = sort_by asc  $ prep txt
+                | otherwise                       = sort_by asc  $ prep txt 
+          where 
+                 asc  a b = fst a `compare` fst b
+                 desc a b = fst b `compare` fst a
+
+                 num :: Row -> Maybe Scientific
+                 num row = case HashMap.lookup nm row of
+                            Just (Number n)   -> return n
+                            Just (String txt) -> case reads $ Text.unpack txt of
+                                                   [(a::Scientific,"")] -> Just a
+                                                   _                    -> Nothing
+                            _                 -> Nothing
+
+                 txt :: Row -> Maybe Text
+                 txt row = case HashMap.lookup nm row of
+                            Just (String txt) -> return txt
+                            Just (Number n)   -> return $ Text.pack $ show n
+                            Just (Bool b)     -> return $ Text.pack $ show b
+                            _                 -> Nothing
+
+                 sort_by :: (Ord a) => ((a,b) -> (a,b) -> Ordering) -> [(a,b)] -> [b]
+                 sort_by cmp = map snd . sortBy cmp
+
+                 prep :: forall a . (Row -> a) -> [(a,Row)]                      
+                 prep p = map (\ v -> (p v,v)) rows
+                              
+
+--rows\ a b -> fst a `compare` fst b)                
+--
+--        sel rows (TAKE n)        = Prelude.take n rows
+--        sel rows (DROP n)        = Prelude.drop n rows
+
+
+data SORT_MOD = NUM | DESC
+        deriving (Eq, Ord, Show, Read)
+
