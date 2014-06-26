7bd5d10093fefe6e590d76a50e7ab49dca425a43
Andy Gill <andygill@ku.edu>
2014-06-26 15:08:34 -0500

    Fixing scotty crud

--- scotty-crud/Web/Scotty/CRUD.hs
+++ scotty-crud/Web/Scotty/CRUD.hs
@@ -1,25 +1,38 @@
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 module Web.Scotty.CRUD where
 
-import Data.Aeson
-import Web.Scotty
+import Data.Aeson hiding (json)
+import Web.Scotty 
 import qualified Data.HashMap.Strict as HashMap
-
-import Web.CRUD
+import Control.Monad.IO.Class (liftIO) 
+import Data.CRUD
+import Data.Monoid
 
 -- | scottyCRUD provides scotty support for a CRUD object.
 -- 
 -- > crud <- liftIO $ persistantCRUD "filename"
 -- > scottyCRUD "URL" (atomicCRUD crud)
 
-scottyCRUD :: (FromJSON row, ToJSON row) => String -> CRUD IO row -> ScottyM ()
+scottyCRUD :: (Show row, FromJSON row, ToJSON row) => String -> CRUD IO row -> ScottyM ()
 scottyCRUD url crud = do
 
+        post (capture url) $ do
+                dat <- jsonData
+                namedRow <- liftIO $ createRow crud dat
+                json namedRow
+
         get (capture url) $ do 
-                tab <- getTable crud 
-                liftIO $ print tab        
-                return ()
+                tab <- liftIO $ getTable crud
+                json $ [ Named k v | (k,v) <- HashMap.toList $ tab ]
         
+        get (capture (url <> "/:id")) $ do 
+                iD <- param "id"
+                opt_row <- liftIO $ getRow crud iD
+                case opt_row of
+                  Nothing -> next
+                  Just namedRow -> json $ namedRow
+                liftIO $ print $ opt_row
+
 --        get (capture $ url ++ "/:id") $ do return ()
 
                 
-
--- scotty-crud/example/Main.hs
+++ scotty-crud/example/Main.hs
@@ -1,5 +1,23 @@
-
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 
 import Web.Scotty
+import Data.Aeson
+import Data.HashMap.Strict as HashMap
+import Control.Monad.IO.Class (liftIO) 
 
-main = print ()
+import Web.Scotty.CRUD
+import Data.CRUD
+
+import Data.Monoid (mconcat)
+
+main = scotty 3000 $ do
+
+  users  <- liftIO $ actorCRUD (\ _ -> return ())
+                      (HashMap.fromList [("foo",HashMap.fromList [("x", Number 123)])] :: Table Row)
+
+  scottyCRUD "/users" (atomicCRUD users)
+
+  get "/:word" $ do
+    beam <- param "word"
+    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+
