cbc1438c4accdbebb7442e4b836a2957a71b24fa
Andy Gill <andygill@ku.edu>
2014-08-18 22:56:28 -0500

    Adding example for showing a table.

--- examples/Service.hs
+++ examples/Service.hs
@@ -3,6 +3,9 @@ module Main where
 
 import Control.Monad.IO.Class (liftIO) 
 
+import Data.Monoid
+
+import Data.Text (Text, pack)
 import Data.Aeson
 import Data.HashMap.Strict as HashMap
 
@@ -14,7 +17,13 @@ main :: IO ()
 main = scotty 3000 $ do
 
   let tab :: Table Row
-      tab = HashMap.fromList [("foo",HashMap.fromList [("firstname", "Roger"),("lastname","Rabbit"),("age", Number 21)])]
+      tab = HashMap.fromList $
+      	    [("foo",HashMap.fromList [("firstname", "Roger"),("lastname","Rabbit"),("age", Number 21)])
+	    ,("abc",HashMap.fromList [("firstname", "Roger"),("lastname","Rabbit"),("bla",Bool True)])
+	    ] ++
+	    [ ("abc-" <> pack (show n),HashMap.fromList [("firstname", "Roger"),("lastname","Rabbit"),("bla",Bool False)])
+	    | n <- [1..100]
+	    ]
 
   users  <- liftIO $ actorCRUD 
   	    	      (\ _ -> return ())	-- do not store the updates anywhere
